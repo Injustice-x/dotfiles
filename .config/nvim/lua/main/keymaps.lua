e91a54fcfffb93bd3ba9de53f121bdf32474326e
sumanhosad <sumanhosad98@gmail.com>
2025-02-03 08:59:12 +0530

    Mon Feb  3 08:59:12 AM IST 2025 added macro for deleting without yanking to register

--- .config/nvim/lua/main/keymaps.lua
+++ .config/nvim/lua/main/keymaps.lua
@@ -12,6 +12,8 @@ keymap("v", "<leader>d", '"_d')
 -- Clear search highlighting
 keymap("n", "<leader>nh", ":nohlsearch<CR>")
 
+keymap("v", "<leader>p", '"_dp')
+
 -- Split windows
 keymap("n", "<leader>sv", ":vsplit<CR>")
 keymap("n", "<leader>sh", ":split<CR>")
