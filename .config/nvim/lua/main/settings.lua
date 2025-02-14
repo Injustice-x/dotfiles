e629fd8e0209d67c435749181e5939441bbfad93
injustice-x <sumanhosad98@gmail.com>
2025-02-14 20:45:33 +0530

    chore: changed some settings

--- .config/nvim/lua/main/settings.lua
+++ .config/nvim/lua/main/settings.lua
@@ -2,3 +2,25 @@ vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.showmatch = true
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.shiftwidth = 4
+
+vim.opt.updatetime = 300
+vim.opt.wildmode = "longest,list"
+vim.opt.colorcolumn = "80" -- replaces vim.opt.cc=80
+vim.opt.cursorline = true
+
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
+vim.opt.signcolumn = "yes"
