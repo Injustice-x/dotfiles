57a34d914249db53d8b00985f9edbdb57a5e9bda
injustice-x <sumanhosad98@gmail.com>
2025-02-14 20:30:00 +0530

    chore: chnaged config

--- .config/nvim/lua/main/settings.lua
+++ .config/nvim/lua/main/settings.lua
@@ -1,7 +1,4 @@
-vim.opt.showmatch = true
-vim.opt.ignorecase = true
 vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.colorcolumn = "80"
 vim.opt.clipboard = "unnamedplus"
